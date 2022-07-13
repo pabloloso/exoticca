2115d89ee8bb311d5b41cc07489f861d6b4c7796
Pablo López Sorroche <76900130+pabloloso@users.noreply.github.com>
2022-07-13 19:26:26 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Home = styled.div``;
+
+const Title = styled.h1`
+  font-size: 2rem;
+`;
+
+const Text = styled.div`
+  font-size: 1rem;
+  color: #2a2a2a;
+`;
+
+const Link = styled.a`
+  font-weight: 600;
+  color: #2a2a2a;
+`;
+
+export default function App() {
+  return (
+    <Home>
+      <Title>Hi there! :)</Title>
+      <Text>
+        Use the mock of data (you can find it in ./mocks/home.json) from{" "}
+        <Link href="https://api-us.exoticca.com/api/home" target="_blank">
+          https://api-us.exoticca.com/api/home
+        </Link>{" "}
+        and show the cards as we do it in{" "}
+        <Link href="https://www.exoticca.com/us" target="_blank">
+          https://www.exoticca.com/us
+        </Link>
+      </Text>
+    </Home>
+  );
+}
